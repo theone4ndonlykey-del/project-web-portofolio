0a6e5aad7105ff3c0c34bd0bff733d652e61879f
theone4ndonlykey-del <theone4ndonlykey@gmail.com>
2026-09-01 13:53:51 +0700

    add 80% progress

---
+++ index2.html/index.js
@@ -0,0 +1,9 @@
+const myFirstName = "usamah";
+const myLastName = "mah";
+const umur = 18;
+const ipk = 3.5;
+
+console.log(`haloo ${myFirstName} ${myLastName} saya dari bumi berumur ${umur}`)
+
+
+
